bafc693d5b294da55dd516add284ccb5f498a975
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2020-11-24 01:31:24 -0700

    styled and completed UI (Tables)

--- src/renderer/js/state/StateManager.js
+++ src/renderer/js/state/StateManager.js
@@ -12,10 +12,6 @@ function load_settings_page () {
     if(current_page != "settings") {
         hide_all_pages();
         settings_page.classList.remove("hidden");
-        settings_page.classList.add("animate-in")
-        setTimeout(() => {
-            settings_page.classList.remove("animate-in");
-        }, 2000)
         current_page = "settings"
     }
     document.getElementById("settings").classList.add("active-link");
@@ -29,10 +25,11 @@ function load_cpu_page () {
     if(current_page != "cpu") {
         hide_all_pages();
         cpu_page.classList.remove("hidden");
-        cpu_page.classList.add("animate-in")
+        /*cpu_page.classList.add("animate-in")
         setTimeout(() => {
             cpu_page.classList.remove("animate-in");
         }, 2000)
+        */
         current_page = "cpu"
     }
     document.getElementById("cpu").classList.add("active-link")
