f920ccca888a9a8ccf88987ca4d55a646d267b00
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2020-12-25 16:33:22 -0700

    fixed dark-mode toggle not setting correctly on startup

--- src/renderer/js/settings/dark_mode.js
+++ src/renderer/js/settings/dark_mode.js
@@ -4,13 +4,21 @@ document.onkeypress = function (e) {
     let is_dark_mode = localStorage.getItem("dark-mode");
 
     e = e || window.event;
-
     console.log(e.keyCode)
     if (e.keyCode == 4) {
-        document.getElementById("dark-mode").toggleAttribute("checked")
         change_dark_theme(is_dark_mode)
     }
 
+
+
+}
+
+function toggle_dark_check (isDarkOpposite) {
+    if (isDarkOpposite == "true") {
+        document.getElementById("dark-mode").checked = false;
+    } else {
+        document.getElementById("dark-mode").checked = true;
+    }
 }
 
 function change_dark_theme (is_dark_mode) {
@@ -32,23 +40,24 @@ function change_dark_theme (is_dark_mode) {
         q.classList.toggle("invert-text-dark")
     })
 
+    toggle_dark_check(is_dark_mode)
 
 }
 
 function change_dark_theme_setting () {
     let is_dark_mode = localStorage.getItem("dark-mode");
-    document.getElementById("dark-mode").toggleAttribute("checked")
     change_dark_theme (is_dark_mode);
 }
 
 function set_default_dark_mode () {
     let is_dark_mode = localStorage.getItem("dark-mode");
 
-    if (is_dark_mode == "true"){
-        document.getElementById("dark-mode").toggleAttribute("checked")
-    } else {
+    if (is_dark_mode == "false"){
         change_dark_theme(is_dark_mode)
+    } else {
+        document.getElementById("dark-mode").checked = true;
     }
+
 
 }
 
